c4866c4a3a4db1312fe2a1facf5e5e652ebac723
veqox <sonnleitner.luki@gmail.com>
2024-02-11 18:04:18 +0100

    refactor: move back to standard postgres (with pg_cron and pg_partman currently not working)

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM postgres:latest
+
+ENV POSTGRES_PASSWORD=postgres
+ENV POSTGRES_USER=postgres
+ENV POSTGRES_DB=ao
+
+EXPOSE 5432
+
+RUN apt update && apt install -y \
+    git \
+    make \
+    gcc \
+    libpq-dev \
+    postgresql-server-dev-16
+
+RUN git clone https://github.com/pgpartman/pg_partman.git \
+    && cd pg_partman \
+    && make \
+    && make install
+
+RUN git clone https://github.com/citusdata/pg_cron.git \
+    && cd pg_cron \
+    && make \
+    && make install
+
+COPY ./postgresql.conf /etc/postgresql/postgresql.conf
+COPY ./init.sql /docker-entrypoint-initdb.d/init.sql
--- init.sql
+++ init.sql
@@ -1,18 +1,17 @@
--- ######### EXTENSIONS #########
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE EXTENSION IF NOT EXISTS timescaledb;
+--CREATE EXTENSION IF NOT EXISTS pg_partman;
+--CREATE EXTENSION IF NOT EXISTS pg_cron;
 
--- ########### TABLES ###########
 CREATE TABLE
     IF NOT EXISTS item (
         unique_name TEXT NOT NULL,
-        id INTEGER NOT NULL UNIQUE,
+        id INTEGER NOT NULL,
         PRIMARY KEY (unique_name)
     );
 
 CREATE TABLE
     IF NOT EXISTS localized_name (
-        item_unique_name TEXT NOT NULL,
+        item_unique_name TEXT UNIQUE NOT NULL,
         en_us TEXT,
         de_de TEXT,
         fr_fr TEXT,
@@ -70,15 +69,14 @@ CREATE TABLE
         timestamp TIMESTAMPTZ NOT NULL,
         item_amount INTEGER NOT NULL,
         silver_amount INTEGER NOT NULL,
-        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
         PRIMARY KEY (
             item_unique_name,
             location_id,
             quality_level,
             timescale,
-            timestamp,
-            updated_at
+            timestamp
         ),
         FOREIGN KEY (item_unique_name) REFERENCES item (unique_name),
         FOREIGN KEY (location_id) REFERENCES location (id)
@@ -95,55 +93,26 @@ CREATE TABLE
         amount INTEGER NOT NULL,
         auction_type TEXT NOT NULL,
         expires_at TIMESTAMPTZ NOT NULL,
-        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-        PRIMARY KEY (id, updated_at),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        PRIMARY KEY (id),
         FOREIGN KEY (item_unique_name) REFERENCES item (unique_name),
         FOREIGN KEY (location_id) REFERENCES location (id)
     );
 
--- ########### INDEXES ##########
-CREATE INDEX ON market_order (id, item_unique_name, location_id, expires_at, updated_at, created_at);
-CREATE INDEX ON market_history (item_unique_name, location_id, timescale, updated_at, created_at);
-
--- ######### HYPERTABLES ########
-SELECT
-    create_hypertable(
-        'market_history',
-        'updated_at',
-        chunk_time_interval => INTERVAL '1 day',
-        if_not_exists => true
-    );
-
-SELECT
-    create_hypertable(
-        'market_order',
-        'updated_at',
-        chunk_time_interval => INTERVAL '1 day',
-        if_not_exists => true
-    );
-
-    
--- ##### MATERIALIZED VIEWS #####
 CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_count_by_location AS
 SELECT
     location_id,
     COUNT(*) as count
 FROM
-    market_order,
-    location
-WHERE
-    location_id = location.id
+    market_order
 GROUP BY
     location_id
 ORDER BY
     count DESC;
 
--- ### CONTINUOUS AGGREGATIONS ###
-CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_updated_in_last_24h 
-WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_updated_in_last_24h AS
 SELECT
-    time_bucket('1 day', updated_at) as day,
     id,
     item_unique_name,
     location_id,
@@ -158,226 +127,94 @@ SELECT
 FROM
     market_order
 WHERE
-    updated_at > NOW() - INTERVAL '1 day'
-GROUP BY
-    time_bucket('1 day', updated_at),
-    id,
-    item_unique_name,
-    location_id,
-    quality_level,
-    enchantment_level,
-    unit_price_silver,
-    amount,
-    auction_type,
-    expires_at,
-    updated_at,
-    created_at;
+    updated_at > NOW() - INTERVAL '1 day';
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_count_by_updated_at
-WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS market_order_stats_by_item_and_day AS
 SELECT
-    time_bucket('1 hour', updated_at) as updated_at,
+    updated_at::date as date,
+    item_unique_name,
+    COUNT(*) as count,
+    MAX(unit_price_silver) as max_unit_price_silver,
+    MIN(unit_price_silver) as min_unit_price_silver,
+    AVG(unit_price_silver) as avg_unit_price_silver,
+    SUM(amount) as sum_amount
+FROM
+    market_order
+GROUP BY
+    updated_at::date,
+    item_unique_name;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_count_by_updated_at AS
+SELECT
+    date_trunc('hour', updated_at) as updated_at,
     COUNT(*) as count
 FROM
     market_order
 WHERE
     expires_at > NOW()
 GROUP BY
-    time_bucket('1 hour', updated_at)
+    date_trunc('hour', updated_at)
 ORDER BY
     updated_at DESC;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_count_by_updated_at_and_location
-WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_count_by_updated_at_and_location AS
 SELECT
-    time_bucket('1 hour', updated_at) as updated_at,
-    location.name as location,
+    date_trunc('hour', updated_at)  as updated_at,
+    location_id,
     COUNT(*) as count
 FROM
     market_order
-    JOIN location ON location_id = location.id
 WHERE
     expires_at > NOW ()
 GROUP BY
-    time_bucket('1 hour', updated_at),
-    location.name
+    date_trunc('hour', updated_at),
+    location_id
 ORDER BY
     updated_at DESC;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS market_history_updated_in_last_24h
-WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_count_by_created_at AS
 SELECT
-    time_bucket('1 day', updated_at) as day,
-    item_unique_name,
-    location_id,
-    quality_level,
-    timescale,
-    timestamp,
-    item_amount,
-    silver_amount,
-    created_at,
-    updated_at
-FROM
-    market_history
-WHERE
-    updated_at > NOW() - INTERVAL '1 day'
-GROUP BY
-    time_bucket('1 day', updated_at),
-    item_unique_name,
-    location_id,
-    quality_level,
-    timescale,
-    timestamp,
-    item_amount,
-    silver_amount,
-    created_at,
-    updated_at;
-
-CREATE MATERIALIZED VIEW IF NOT EXISTS market_history_count_by_updated_at
-WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS
-SELECT
-    time_bucket('1 hour', updated_at) as updated_at,
-    COUNT(*) as count
-FROM
-    market_history
-GROUP BY
-    time_bucket('1 hour', updated_at)
-ORDER BY
-    updated_at DESC;
-
-CREATE MATERIALIZED VIEW IF NOT EXISTS market_history_count_by_updated_at_and_location
-WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS
-SELECT
-    time_bucket('1 hour', updated_at) as updated_at,
-    location.name as location,
-    COUNT(*) as count
-FROM
-    market_history
-    JOIN location ON location_id = location.id
-GROUP BY
-    time_bucket('1 hour', updated_at),
-    location.name
-ORDER BY
-    updated_at DESC;
-
--- ####### REFRESH POLICIES #######
-SELECT add_continuous_aggregate_policy(
-    'market_orders_updated_in_last_24h',
-    start_offset => NULL,
-    end_offset => NULL,
-    schedule_interval => INTERVAL '5 minutes');
-
-SELECT add_continuous_aggregate_policy(
-    'market_orders_count_by_updated_at',
-    start_offset => NULL,
-    end_offset => NULL,
-    schedule_interval => INTERVAL '5 minutes');
-
-SELECT add_continuous_aggregate_policy(
-    'market_orders_count_by_updated_at_and_location',
-    start_offset => NULL,
-    end_offset => NULL,
-    schedule_interval => INTERVAL '5 minutes');
-
-SELECT add_continuous_aggregate_policy(
-    'market_history_updated_in_last_24h',
-    start_offset => NULL,
-    end_offset => NULL,
-    schedule_interval => INTERVAL '5 minutes');
-
-SELECT add_continuous_aggregate_policy(
-    'market_history_count_by_updated_at',
-    start_offset => NULL,
-    end_offset => NULL,
-    schedule_interval => INTERVAL '5 minutes');
-
-SELECT add_continuous_aggregate_policy(
-    'market_history_count_by_updated_at_and_location',
-    start_offset => NULL,
-    end_offset => NULL,
-    schedule_interval => INTERVAL '5 minutes');
-
-/*
-CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_count_by_created_at
-WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS
-SELECT
-    time_bucket('1 hour', created_at) as created_at,
+    date_trunc('hour', created_at) as created_at,
     COUNT(*) as count
 FROM
     market_order
 WHERE
     expires_at > NOW()
 GROUP BY
-    time_bucket('1 hour', created_at)
+    date_trunc('hour', created_at)
 ORDER BY
     created_at DESC;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_count_by_created_at_and_location
-WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_count_by_created_at_and_location AS
 SELECT
-    time_bucket('1 hour', created_at) as created_at,
-    location.name as location,
+    date_trunc('hour', created_at)  as created_at,
+    location_id,
     COUNT(*) as count
 FROM
     market_order
-    JOIN location ON location_id = location.id
 WHERE
     expires_at > NOW ()
 GROUP BY
-    time_bucket('1 hour', created_at),
-    location.name
+    date_trunc('hour', created_at),
+    location_id
 ORDER BY
     created_at DESC;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS market_orders_count_by_created_at_and_location
-WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS
-SELECT
-    time_bucket('1 hour', created_at) as created_at,
-    location.name as location,
-    COUNT(*) as count
-FROM
-    market_order
-    JOIN location ON location_id = location.id
-WHERE
-    expires_at > NOW ()
-GROUP BY
-    time_bucket('1 hour', created_at),
-    location.name
-ORDER BY
-    created_at DESC;
+CREATE UNIQUE INDEX ON market_order_stats_by_item_and_day (date, item_unique_name);
+CREATE UNIQUE INDEX ON market_orders_count_by_created_at (created_at);
+CREATE UNIQUE INDEX ON market_orders_count_by_created_at_and_location (created_at, location_id);
+CREATE UNIQUE INDEX ON market_orders_count_by_updated_at (updated_at);
+CREATE UNIQUE INDEX ON market_orders_count_by_updated_at_and_location (updated_at, location_id);
+CREATE UNIQUE INDEX ON market_orders_count_by_location (location_id);
+CREATE UNIQUE INDEX ON market_orders_updated_in_last_24h (id);
 
+-- Debug to check if extensions are installed
+SELECT * FROM pg_extension;
 
-SELECT add_continuous_aggregate_policy(
-    'market_orders_count_by_created_at',
-    start_offset => NULL,
-    end_offset => NULL,
-    schedule_interval => INTERVAL '5 minutes');
-
-SELECT add_continuous_aggregate_policy(
-    'market_orders_count_by_created_at_and_location',
-    start_offset => NULL,
-    end_offset => NULL,
-    schedule_interval => INTERVAL '5 minutes');
-*/
-
-/*
-REFRESH MATERIALIZED VIEW markSELECT create_hypertable('market_history', 'timestamp', chunk_time_interval => INTERVAL '1 day');et_orders_count_by_location
-WITH
-NO DATA;
-
-REFRESH MATERIALIZED VIEW market_orders_count_by_updated_at
-WITH
-NO DATA;
-
-REFRESH MATERIALIZED VIEW market_orders_count_by_updated_at_and_location
-WITH
-NO DATA;
-
-REFRESH MATERIALIZED VIEW market_orders_count_by_created_at
-WITH
-NO DATA;
-
-REFRESH MATERIALIZED VIEW market_orders_count_by_created_at_and_location
-WITH
-NO DATA;
- */
+REFRESH MATERIALIZED VIEW CONCURRENTLY market_order_stats_by_item_and_day;
+REFRESH MATERIALIZED VIEW CONCURRENTLY market_orders_count_by_created_at;
+REFRESH MATERIALIZED VIEW CONCURRENTLY market_orders_count_by_created_at_and_location;
+REFRESH MATERIALIZED VIEW CONCURRENTLY market_orders_count_by_updated_at;
+REFRESH MATERIALIZED VIEW CONCURRENTLY market_orders_count_by_updated_at_and_location;
+REFRESH MATERIALIZED VIEW CONCURRENTLY market_orders_count_by_location;
+REFRESH MATERIALIZED VIEW CONCURRENTLY market_orders_updated_in_last_24h;
