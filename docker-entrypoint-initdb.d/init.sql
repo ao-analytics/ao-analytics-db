9f24999f4cdb4ade1c78f06593da5be8e3ca1912
veqox <veqoxao@gmail.com>
2025-02-06 23:02:31 +0100

    refactor: redo db schema feat: decouple nats data from additional meta data todo: migration script for v0.2.0

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -5,92 +5,107 @@ CREATE EXTENSION IF NOT EXISTS timescaledb;
 SET pg_trgm.similarity_threshold = 0.5;
 
 CREATE TABLE
+    IF NOT EXISTS item_group (
+        name TEXT NOT NULL,
+        --
+        PRIMARY KEY (name)
+);
+
+CREATE TABLE
     IF NOT EXISTS item (
         unique_name TEXT NOT NULL,
-        PRIMARY KEY (unique_name)
+        --
+        item_group_name TEXT NOT NULL,
+        enchantment_level SMALLINT NOT NULL,
+        --
+        PRIMARY KEY (unique_name),
+        FOREIGN KEY (item_group_name) REFERENCES item_group (name)
 );
 
 CREATE TABLE
     IF NOT EXISTS
         item_data (
-            item_unique_name TEXT NOT NULL,
+            item_group_name TEXT NOT NULL,
+            --
             data JSONB NOT NULL,
-
-            PRIMARY KEY (item_unique_name),
-            FOREIGN KEY (item_unique_name) REFERENCES item (unique_name)
+            --
+            PRIMARY KEY (item_group_name),
+            FOREIGN KEY (item_group_name) REFERENCES item_group (name)
 );
 
 CREATE TABLE
     IF NOT EXISTS localized_name (
         item_unique_name TEXT NOT NULL,
         lang TEXT NOT NULL,
+        --
         name TEXT NOT NULL,
+        --
         PRIMARY KEY (item_unique_name, lang),
         FOREIGN KEY (item_unique_name) REFERENCES item (unique_name)
 );
-
-CREATE INDEX IF NOT EXISTS localized_name_lang_idx ON localized_name (lang);
-CREATE INDEX IF NOT EXISTS localized_name_name_idx ON localized_name (name);
-CREATE INDEX localized_name_lang_gist_idx ON localized_name USING gist(name gist_trgm_ops);
 
 CREATE TABLE
     IF NOT EXISTS localized_description (
         item_unique_name TEXT NOT NULL,
         lang TEXT NOT NULL,
+        --
         description TEXT NOT NULL,
+        --
         PRIMARY KEY (item_unique_name, lang),
         FOREIGN KEY (item_unique_name) REFERENCES item (unique_name)
 );
 
-CREATE INDEX localized_description_lang_idx ON localized_description (lang);
-CREATE INDEX localized_description_description_idx ON localized_description (description);
-
 CREATE TABLE
     IF NOT EXISTS location (
-        id TEXT NOT NULL,
-        name TEXT NOT NULL,
+        id SMALLINT NOT NULL,
+        ---
         PRIMARY KEY (id)
-    );
+);
+
+CREATE TABLE
+    IF NOT EXISTS location_data (
+        id TEXT NOT NULL,
+        --
+        location_id SMALLINT,
+        name TEXT NOT NULL,
+        --
+        PRIMARY KEY (id),
+        FOREIGN KEY (location_id) REFERENCES location (id),
+        UNIQUE(id, location_id)
+);
 
 CREATE TABLE
     IF NOT EXISTS market_order (
         id BIGINT NOT NULL,
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        --
         item_unique_name TEXT NOT NULL,
-        location_id TEXT NOT NULL,
-        tier INTEGER NOT NULL,
-        enchantment_level INTEGER NOT NULL,
-        quality_level INTEGER NOT NULL,
-        unit_price_silver INTEGER NOT NULL,
-        amount INTEGER NOT NULL,
+        location_id SMALLINT NOT NULL,
+        quality_level SMALLINT NOT NULL,
+        unit_price_silver BIGINT NOT NULL,
+        amount BIGINT NOT NULL,
         auction_type TEXT NOT NULL,
         expires_at TIMESTAMPTZ NOT NULL,
-        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        --
         PRIMARY KEY (id, updated_at),
         FOREIGN KEY (item_unique_name) REFERENCES item (unique_name),
         FOREIGN KEY (location_id) REFERENCES location (id)
-    );
+);
 
 SELECT FROM create_hypertable('market_order', by_range('updated_at', INTERVAL '30 minutes'), if_not_exists := true);
-
-CREATE INDEX IF NOT EXISTS market_order_id_idx ON market_order (id);
-CREATE INDEX IF NOT EXISTS market_order_location_id_idx ON market_order (location_id);
-CREATE INDEX IF NOT EXISTS market_order_tier_idx ON market_order (tier);
-CREATE INDEX IF NOT EXISTS market_order_enchantment_level_idx ON market_order (enchantment_level);
-CREATE INDEX IF NOT EXISTS market_order_quality_level_idx ON market_order (quality_level);
-CREATE INDEX IF NOT EXISTS market_order_auction_type_idx ON market_order (auction_type);
 
 CREATE TABLE
     IF NOT EXISTS market_history (
         item_unique_name TEXT NOT NULL,
-        location_id TEXT NOT NULL,
-        tier INTEGER NOT NULL,
-        enchantment_level INTEGER NOT NULL,
-        quality_level INTEGER NOT NULL,
-        timescale INTEGER NOT NULL,
+        location_id SMALLINT NOT NULL,
+        quality_level SMALLINT NOT NULL,
+        timescale SMALLINT NOT NULL,
         timestamp TIMESTAMPTZ NOT NULL,
-        item_amount INTEGER NOT NULL,
-        silver_amount INTEGER NOT NULL,
+        --
+        item_amount BIGINT NOT NULL,
+        silver_amount BIGINT NOT NULL,
         updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        --
         PRIMARY KEY (
             item_unique_name,
             location_id,
@@ -100,17 +115,9 @@ CREATE TABLE
         ),
         FOREIGN KEY (item_unique_name) REFERENCES item (unique_name),
         FOREIGN KEY (location_id) REFERENCES location (id)
-    );
+);
 
 SELECT FROM create_hypertable('market_history', by_range('timestamp', INTERVAL '1 day'), if_not_exists := true);
-
-CREATE INDEX IF NOT EXISTS market_history_timescale_idx ON market_history (timescale);
-CREATE INDEX IF NOT EXISTS market_history_timestamp_idx ON market_history (timestamp);
-CREATE INDEX IF NOT EXISTS market_history_item_unique_name_idx ON market_history (item_unique_name);
-CREATE INDEX IF NOT EXISTS market_history_location_id_idx ON market_history (location_id);
-CREATE INDEX IF NOT EXISTS market_history_tier_idx ON market_history (tier);
-CREATE INDEX IF NOT EXISTS market_history_enchantment_level_idx ON market_history (enchantment_level);
-CREATE INDEX IF NOT EXISTS market_history_quality_level_idx ON market_history (quality_level);
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS item_prices_by_hour_and_location
 WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS
@@ -121,11 +128,11 @@ SELECT
     COUNT(*) as total_count,
     MAX(unit_price_silver) FILTER (WHERE auction_type = 'offer') as max_unit_price_silver_offer,
     MIN(unit_price_silver) FILTER (WHERE auction_type = 'offer') as min_unit_price_silver_offer,
-    ROUND(SUM(unit_price_silver::BIGINT * amount::BIGINT) FILTER (WHERE auction_type = 'offer') / SUM(amount) FILTER (WHERE auction_type = 'offer'))::INTEGER as avg_unit_price_silver_offer,
+    ROUND(SUM(unit_price_silver * amount) FILTER (WHERE auction_type = 'offer') / SUM(amount) FILTER (WHERE auction_type = 'offer'))::BIGINT as avg_unit_price_silver_offer,
     SUM(amount) FILTER (WHERE auction_type = 'offer') as sum_amount_offer,
     MAX(unit_price_silver) FILTER (WHERE auction_type = 'request') as max_unit_price_silver_request,
     MIN(unit_price_silver) FILTER (WHERE auction_type = 'request') as min_unit_price_silver_request,
-    ROUND(SUM(unit_price_silver::BIGINT * amount::BIGINT) FILTER (WHERE auction_type = 'request') / SUM(amount) FILTER (WHERE auction_type = 'request'))::INTEGER as avg_unit_price_silver_request,
+    ROUND(SUM(unit_price_silver * amount) FILTER (WHERE auction_type = 'request') / SUM(amount) FILTER (WHERE auction_type = 'request'))::BIGINT as avg_unit_price_silver_request,
     SUM(amount) FILTER (WHERE auction_type = 'request') as sum_amount_request
 FROM
     market_order
@@ -142,11 +149,11 @@ SELECT
     COUNT(*) as total_count,
     MAX(unit_price_silver) FILTER (WHERE auction_type = 'offer') as max_unit_price_silver_offer,
     MIN(unit_price_silver) FILTER (WHERE auction_type = 'offer') as min_unit_price_silver_offer,
-    ROUND(SUM(unit_price_silver::BIGINT * amount::BIGINT) FILTER (WHERE auction_type = 'offer') / SUM(amount) FILTER (WHERE auction_type = 'offer'))::INTEGER as avg_unit_price_silver_offer,
+    ROUND(SUM(unit_price_silver * amount) FILTER (WHERE auction_type = 'offer') / SUM(amount) FILTER (WHERE auction_type = 'offer'))::BIGINT as avg_unit_price_silver_offer,
     SUM(amount) FILTER (WHERE auction_type = 'offer') as sum_amount_offer,
     MAX(unit_price_silver) FILTER (WHERE auction_type = 'request') as max_unit_price_silver_request,
     MIN(unit_price_silver) FILTER (WHERE auction_type = 'request') as min_unit_price_silver_request,
-    ROUND(SUM(unit_price_silver::BIGINT * amount::BIGINT) FILTER (WHERE auction_type = 'request') / SUM(amount) FILTER (WHERE auction_type = 'request'))::INTEGER as avg_unit_price_silver_request,
+    ROUND(SUM(unit_price_silver * amount) FILTER (WHERE auction_type = 'request') / SUM(amount) FILTER (WHERE auction_type = 'request'))::BIGINT as avg_unit_price_silver_request,
     SUM(amount) FILTER (WHERE auction_type = 'request') as sum_amount_request
 FROM
     market_order
@@ -227,4 +234,3 @@ SELECT add_compression_policy(
        if_not_exists := true,
        schedule_interval := INTERVAL '1 day',
        initial_start := NULL);
-
